b61664e34afbd181c1fcf8786597ab7ec0d09220
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2022-08-19 23:57:31 +0530

    feat: health check routes

--- src/core/web/gin.go
+++ src/core/web/gin.go
@@ -11,10 +11,24 @@ func Run() {
 	router := gin.Default()
 	route.MountRoutes(router)
 
+	// Health Checks
+	router.POST("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusCreated, gin.H{
+			"msg": "OK",
+		})
+	})
+
+	router.GET("/healthcheck", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "OK",
+		})
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
 
 	server.ListenAndServe()
+
 }
